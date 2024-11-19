11caedfe7f7bdfa8bc4419c68778f6b7115a0f1b
Moattar Ansari <moattaransari62@gmail.com>
2024-11-19 07:44:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+"use strict";
+const funcName = () => {
+    console.log("Hello Moattar");
+};
+funcName();
+funcName();
+funcName();
+funcName();
+funcName();
+funcName();
+funcName();
+funcName();
+funcName();
+// Another function...
+const anOtherfunc = () => {
+    funcName;
+};
+anOtherfunc();
+// NesTed FuNctiOn..
+const newFunc = () => {
+    let Data = "I am Perfect";
+    return Data;
+};
+console.log(newFunc());
+// sUm..
+let sum = () => {
+    return 7 + 8;
+};
+let total = sum();
+console.log((total));
+// PracTise ....
+let PersonsData = () => {
+    let Name = "Moattar \n";
+    let Age = 17;
+    return Name + Age;
+};
+console.log(PersonsData());
+let arrowfunction = () => {
+    let data = "Moattar Ansari";
+    return data;
+};
+console.log(arrowfunction());
+const arrowfunc = () => {
+    let me = "Moattar Ansari \n";
+    let intro = "I am passionate Webdeveloper";
+    return me + intro;
+};
+console.log(arrowfunc());
+const myFamily = () => {
+    let Baba = "Zubair";
+    let Mama = "Noreen";
+    let bhai = "Ahmad";
+    let me = "Moattar";
+    let sis = "Manal";
+    let sis2 = "khazina";
+    return;
+};
---
+++ index.ts
@@ -0,0 +1,78 @@
+const funcName = () => {
+    console.log("Hello Moattar")
+}
+
+funcName()
+funcName()
+funcName()
+funcName()
+funcName()
+funcName()
+funcName()
+funcName()
+funcName()
+
+// Another function...
+const anOtherfunc = () => {
+    funcName
+}
+anOtherfunc()
+
+
+// NesTed FuNctiOn..
+
+const newFunc = ():string => {
+    let Data:string = "I am Perfect"
+    return Data;
+}
+
+console.log(newFunc())
+
+// sUm..
+
+let sum = () => {
+    return 7+8
+}
+let total = sum()
+
+console.log((total))
+
+
+// PracTise ....
+
+let PersonsData = () => {
+    let Name:string = "Moattar \n";
+    let Age:number =17;
+    return Name + Age;
+}
+console.log(PersonsData());
+
+
+let arrowfunction =() => {
+        let data:string = "Moattar Ansari";
+        return data;
+}
+
+console.log(arrowfunction());
+
+
+const arrowfunc = () => {
+    let me:string = "Moattar Ansari \n";
+    let intro:string = "I am passionate Webdeveloper";
+    return me + intro;
+}
+
+console.log(arrowfunc());
+
+
+const myFamily = () => {
+    let Baba:string = "Zubair";
+    let Mama:string = "Noreen";
+    let bhai:string = "Ahmad";
+    let me:string ="Moattar";
+    let sis:string = "Manal";
+    let sis2:string = "khazina";
+
+return 
+
+}
